5e3d172eb8b9add48d82c67dd03ec911a25dda67
MRpinhe <huangpinhe65@gmail.com>
2025-09-03 22:27:08 +0800

    ThreadBinTree

---
+++ s607-threaded_bin_tree/build/CMakeFiles/s607-threadbintree.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/s607-threadbintree.dir/src/main.cpp.o"
+  "CMakeFiles/s607-threadbintree.dir/src/main.cpp.o.d"
+  "CMakeFiles/s607-threadbintree.dir/src/threadbintree.cpp.o"
+  "CMakeFiles/s607-threadbintree.dir/src/threadbintree.cpp.o.d"
+  "s607-threadbintree"
+  "s607-threadbintree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/s607-threadbintree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
